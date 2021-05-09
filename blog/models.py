560261a4ac11809deff9a7588821bb91bb675baa
abiolarasaq <rasaqabiola25@gmail.com>
2021-05-09 15:03:41 +0100

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=225, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    publish_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+class post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    publish_date = models.DateTimeField(blank=True, null=True)
+
+    def publish (self):
+        self.published_date =timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
